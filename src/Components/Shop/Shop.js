8f28d649fcea557418bb7813d9552056fdcaa4a8
juhayerayaz <aiazj97@gmail.com>
2022-04-06 03:43:41 +0600

    emty

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -54,13 +54,6 @@ const Shop = () => {
                     <button onClick={removeItem} className='btn btn-danger'>Remove Items</button>
                 </div>
             </div>
-            {/* question answers for bonus mark  */}
-            <div className='question-answers text-center mt-5'>
-                <h2>Q1. How React Works?</h2>
-                <p>Ans: React is a JavaScript library. It basically maintains a tree which is able to do efficient diff computations on the node.It works in asynchronous way and does all the work in a first come first serve manner. It defines the logical structure of documents and the way a document is accessed and manipulated.To make it faster, React implements a virtual DOM that is basically a DOM tree representation in JavaScript. </p>
-                <h2>Q2. How useState Works?</h2>
-                <p>Ans: useState is a Hook that allows you to have state variables in functional components. It basically lets you to add state to function components. useState generates a single piece of state in and function associated with it. It can contain any type of data such as object, array, boolean etc.</p>
-            </div>
         </div>
     );
 };
